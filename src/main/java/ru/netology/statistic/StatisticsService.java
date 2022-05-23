002a086613d8ae6e8ca83def2d7c7a6f55e7a809
Марина Безотеческая <bezotecheskaya@mail.ru>
2022-05-23 23:35:51 +0700

    "StatisticsService"

---
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -0,0 +1,63 @@
+package ru.netology.statistic;
+
+public class StatisticsService {
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
+    public long findMax(long[] incomes) {
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if(current_max <= income)
+                current_max = income;
+        return current_max;
+    }
+
+    public long findMin(long[] incomes) {
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+    }
+
+    public long sumIncomes(long[] incomes) {
+        long sum = 0;
+        for (long income : incomes) {
+            sum += income;
+        }
+        return sum;
+    }
+
+    public long averageIncomes(long[] incomes) {
+        long sum = sumIncomes(incomes);
+        long average = 0;
+        long x = incomes.length;
+        average = sum / x;
+        return average;
+    }
+
+    public long belowAverageIncome(long[] incomes) {
+        long average = averageIncomes(incomes);
+        long count = 0;
+        for (int i = 0; i < incomes.length; i++) {
+            if (incomes[i] < average) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public long aboveAverageIncome(long[] incomes) {
+        long average = averageIncomes(incomes);
+        long count = 0;
+        for (int i = 0; i < incomes.length; i++) {
+            if (incomes[i] > average) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,84 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+
+        long actual = service.findMin(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void sumIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12 + 5 + 8 + 4 + 5 + 3 + 8 + 6 + 11 + 11 + 12;
+
+        long actual = service.sumIncomes(incomesInBillions);
+        assertEquals(expected, actual);
+
+    }
+
+   @Test
+
+    public void averageIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = (12 + 5 + 8 + 4 + 5 + 3 + 8 + 6 + 11 + 11 + 12) / 11;
+
+        long actual = service.averageIncomes(incomesInBillions);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void belowAverageIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 5;
+
+        long actual = service.belowAverageIncome(incomesInBillions);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void aboveAverageIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 6;
+
+        long actual = service.aboveAverageIncome(incomesInBillions);
+        assertEquals(expected, actual);
+
+    }
+}
